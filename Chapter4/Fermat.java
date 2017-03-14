1607cb5d4ea8a0ced16a8690f5d322478cabbf7b
Predrag Skakic <skakic.predrag@gmail.com>
2017-03-14 19:55:41 +0100

    raiseToPow method only

---
+++ Chapter4/Fermat.java
@@ -0,0 +1,17 @@
+class Fermat{
+	public static void main(String[] args){
+	
+
+	public static void checkFermat(int a,int b,int c,int n){
+		
+		
+ public static int raiseToPow(int x,int n){
+		int res = 1;
+		for(int i = 0;i < n;i++){
+		    res = res * x;	
+		}
+	    return res;
+	}
+    
+}
+}
