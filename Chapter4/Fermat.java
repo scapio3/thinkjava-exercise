d2bfc2a2f266f403bb4eb531b797e150b7f779de
Predrag Skakic <skakic.predrag@gmail.com>
2017-03-15 15:37:46 +0100

    Fermat exercise  completed,working

--- Chapter4/Fermat.java
+++ Chapter4/Fermat.java
@@ -1,9 +1,22 @@
 class Fermat{
 	public static void main(String[] args){
-	
+		checkFermat(3,4,5,3);
+	}
 
 	public static void checkFermat(int a,int b,int c,int n){
-		
+        if(n > 2 ){
+		    if(raiseToPow(a,n) + raiseToPow(b,n) == raiseToPow(c,n) ){
+			    System.out.println("Holy smokes, Fermat was wrong!");
+		    }
+		    else{
+		        System.out.println("No,that doesn't work.");
+		    }
+            
+        }else{
+            System.out.println("This works when n is 2!");
+        }
+    
+	}
 		
  public static int raiseToPow(int x,int n){
 		int res = 1;
@@ -13,5 +26,4 @@ class Fermat{
 	    return res;
 	}
     
-}
 }
